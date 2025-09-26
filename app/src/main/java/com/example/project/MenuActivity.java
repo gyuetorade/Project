41e71468aec54f1b3f75b3aab7a07eacd5fce91e
hans <hanslester04@gmail.com>
2025-09-26 23:06:22 +0800

    better search

--- app/src/main/java/com/example/project/MenuActivity.java
+++ app/src/main/java/com/example/project/MenuActivity.java
@@ -15,20 +15,12 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
 
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
 
         cart = getSharedPreferences("Cart", MODE_PRIVATE);
-
-        findViewById(R.id.btnAddBurger).setOnClickListener(v -> addToCart("Burger", 120, R.drawable.moi_moi));
-        findViewById(R.id.btnAddFries).setOnClickListener(v -> addToCart("Fries", 60, R.drawable.egg_cucumber));
-        findViewById(R.id.btnAddMilkTea).setOnClickListener(v -> addToCart("Milk Tea", 90, R.drawable.veggie_tomato));
-
-        Button btnViewCart = findViewById(R.id.btnViewCart);
-        btnViewCart.setOnClickListener(v -> startActivity(new Intent(this, CartActivity.class)));
-    }
+        }
 
     private void addToCart(String item, int price, int imageRes) {
         int qty = cart.getInt(item + "_qty", 0) + 1;
